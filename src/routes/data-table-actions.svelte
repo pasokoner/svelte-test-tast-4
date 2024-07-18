601733fc8e3c8c7911495f696895df5a65a49f6a
John Carlo Asilo <jcarloasilo@gmail.com>
2024-07-18 14:14:39 +0800

    add checkbox & icon

---
+++ src/routes/data-table-actions.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+	import Ellipsis from "lucide-svelte/icons/ellipsis";
+	import * as DropdownMenu from "$lib/components/ui/dropdown-menu";
+	import { Button } from "$lib/components/ui/button";
+	import type { Id } from "$lib/typings";
+
+	type Props = {
+		id: Id;
+	};
+
+	let { id }: Props = $props();
+</script>
+
+<DropdownMenu.Root>
+	<DropdownMenu.Trigger asChild let:builder>
+		<Button variant="ghost" builders={[builder]} size="icon" class="relative h-8 w-8 p-0">
+			<span class="sr-only">Open menu</span>
+			<Ellipsis class="h-4 w-4" />
+		</Button>
+	</DropdownMenu.Trigger>
+	<DropdownMenu.Content>
+		<DropdownMenu.Item>View</DropdownMenu.Item>
+		<DropdownMenu.Item>Edit</DropdownMenu.Item>
+		<DropdownMenu.Item>Delete</DropdownMenu.Item>
+	</DropdownMenu.Content>
+</DropdownMenu.Root>
