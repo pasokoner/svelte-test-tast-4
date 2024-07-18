036c9ade75d67a4eabe20962ac87f4ef444b9751
John Carlo Asilo <jcarloasilo@gmail.com>
2024-07-18 14:15:11 +0800

    stream data from server add all comps

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,34 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	import DataTableSkeleton from "./data-table-skeleton.svelte";
+	import DataTable from "./data-table.svelte";
+
+	let { data } = $props();
+</script>
+
+<main class="space-y-[40px] py-20 md:space-y-[60px]">
+	<div class="container max-w-[1440px] space-y-4 md:space-y-6 xl:px-28">
+		<h1 class="text-center text-3xl font-semibold md:text-6xl">
+			Feature-Rich Data Table in SvelteKit 5
+		</h1>
+		<div>
+			<p class="text-[#475467 text-center lg:px-52 xl:px-72">
+				A data table with sorting, checkbox selection, search, filtering, pagination, and
+				loading/error states using SvelteKit 5 and Tailwind CSS.
+			</p>
+		</div>
+	</div>
+
+	{#await data.users}
+		<div class="container max-w-[1440px] px-4 lg:px-28">
+			<DataTableSkeleton />
+		</div>
+	{:then users}
+		<div class="container max-w-[1440px] px-4 lg:px-28">
+			<DataTable data={users} />
+		</div>
+	{:catch error}
+		<div class="container max-w-[1440px] px-4 text-center text-2xl lg:px-28">
+			Error loading users {error.message}
+		</div>
+	{/await}
+</main>
